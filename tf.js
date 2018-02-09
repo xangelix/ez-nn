bd467b07c39df31b4124f5e30b34d0ef4185a7ac
Cody Neiman <neiman1010222@gmail.com>
2018-02-08 23:22:13 -0500

    Added increased functionallity through GUI

--- tf.js
+++ tf.js
@@ -11,6 +11,21 @@ $(() => {
   $('.loading').hide();
   $('.testPic').hide();
 
+  // Logs all package info
+  /*
+  function npmls(cb) {
+    require('child_process').exec('npm ls --json', function(err, stdout, stderr) {
+      if (err) return cb(err);
+      cb(null, JSON.parse(stdout));
+    });
+  }
+  npmls(console.log);
+  */
+
+  function scrollLog() {
+    $('#log').scrollTop = $('#log').scrollHeight;
+  }
+
 
   $('#startTensorBoard').click(() => {
     $('.startTensorBoard').hide();
@@ -35,6 +50,7 @@ $(() => {
           $('.loading').hide();
           $('.createNeuralNetwork').show();
         }
+        $('#log').append(data.toString());
       });
 
       child.on('exit', function (code) {
@@ -43,32 +59,39 @@ $(() => {
   });
 
   $('#testPic').click(() => {
-    var spawn1 = require('child_process').spawn;
-    if (isWin) {
-      child1 = spawn1('cmd', ['/c', `cd tf/ && python -m scripts.label_image
-      --graph=tf_files/retrained_graph.pb
-      --image=tf_files/flower_photos/daisy/21652746_cc379e0eea_m.jpg`]);
-    } else {
-      child1 = spawn1('bash', ['-c', `cd tf/ && python -m scripts.label_image
-      --graph=tf_files/retrained_graph.pb
-      --image=tf_files/flower_photos/daisy/21652746_cc379e0eea_m.jpg`]);
-    }
 
-      child1.stdout.on('data', function (data) {
-        console.log('stdout: ' + data.toString());
-        document.getElementById("log").innerHTML += data.toString();
-      });
+    const {dialog} = require('electron').remote;
 
-      child1.stderr.on('data', function (data) {
-        console.log('stderr: ' + data.toString());
-      });
+    dialog.showOpenDialog({ filters: [ { name: 'JPG Images', extensions: ['jpg'] } ] }, (data) => {
+      var spawn1 = require('child_process').spawn;
+      if (isWin) {
+        console.log(`cd tf/ && python -m scripts.label_image --graph=tf_files/retrained_graph.pb --image=` + data);
+        child1 = spawn1('cmd', ['/c', `cd tf/ && python -m scripts.label_image --graph=tf_files/retrained_graph.pb --image=` + data]);
+      } else {
+        child1 = spawn1('bash', ['-c', `cd tf/ && python -m scripts.label_image
+        --graph=tf_files/retrained_graph.pb
+        --image=${data}`]);
+      }
 
-      child1.on('exit', function (code) {
-        console.log('child process exited with code ' + code.toString());
-      });
+        child1.stdout.on('data', function (data) {
+          console.log('stdout: ' + data.toString());
+          $('#log').append(data.toString());
+          scrollLog();
+        });
+
+        child1.stderr.on('data', function (data) {
+          console.log('stderr: ' + data.toString());
+        });
+
+        child1.on('exit', function (code) {
+          console.log('child process exited with code ' + code.toString());
+        });
+    });
+
   });
 
   $('#createNeuralNetwork').click(() => {
+    $('#createNeuralNetwork').hide();
     var imageSize = '224';
     var architecture = '0.50';
     var steps = '500';
@@ -90,11 +113,19 @@ $(() => {
 
       child2.stdout.on('data', function (data) {
         console.log('stdout: ' + data.toString());
-        document.getElementById("log").innerHTML += data.toString();
+        if (data.toString().includes('Downloading')) {
+          $('#log').append(data.toString() + '<br />');
+        } else {
+          $('#log').append(data.toString());
+        }
+        scrollLog();
+        $('.testPic').show();
       });
 
       child2.stderr.on('data', function (data) {
         console.log('stderr: ' + data.toString());
+        $('#log').append(data.toString());
+        scrollLog();
       });
 
       child2.on('exit', function (code) {
